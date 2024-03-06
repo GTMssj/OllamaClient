c62682a8d5b9043ebf74d1e85cf2e4cadf9b4106
Debian <SteveTao@Steve-Debian.tail1853f.ts.net>
2024-03-06 16:58:23 +0800

    Added /show

--- client.py
+++ client.py
@@ -8,21 +8,48 @@ class client():
 
     def __init__(self):
         self.model = "dolphin-phi"
-        self.host = "http://10.0.0.4:11434/api/chat"
+        self.host = "http://52.229.187.190:11434/api"
 
     def get_input(self):
-        inp = input("  > ")
-        if inp[0] == '/':
+        try:
+            inp = input("\n  > ")
+        except KeyboardInterrupt:
+            print("\n   Exiting...")
+            exit()
+        if not inp:
             exit()
         else:
-            self.msgs.append({
-                                "role": "user", 
-                                "content": inp
-                            })
+            if inp[0] == '/':
+                args = inp.split(" ")
+                args[0] = args[0][1:]
+                if args[0] == 'show' or args[0] == 'list':
+                    r = requests.get(self.host + "/tags")
+                    res = r.json()['models']
+                    print("\nModels:")
+                    print("--------------------------")
+                    for line in res:
+                        print(f"    {line['details']['parameter_size']}  {line['name'].split(':')[0]}")
+                    print("--------------------------")
+                if args[0] == 'set':
+                    self.host = f"http://{args[1]}:{args[2]}/api"
+                    self.draw()
+                if args[0] == 'run':
+                    self.model = args[1]
+                    self.draw()
+                if args[0] == 'help' or args[0] == '?':
+                    self.help()
+                if args[0] == 'exit' or args[0] == 'quit' or args[0] == 'q':
+                    exit()
+            else:
+                self.msgs.append({
+                                    "role": "user", 
+                                    "content": inp
+                                })
+                self.chat()
 
     def chat(self):
         r = requests.post(
-                self.host, 
+                self.host + "/chat", 
                 json = {
                         "model": self.model, 
                         "messages": self.msgs, 
@@ -40,8 +67,6 @@ class client():
                 message = body.get("message", "")
                 content = message.get("content", "")
                 output += content
-                #print(content, end = "")
-                #print(content, end="", flush=True)
             if body.get("done", False):
                 message["content"] = output
         self.msgs.append({
@@ -52,18 +77,32 @@ class client():
     
     def draw(self):
         os.system("clear")
-        print(f"\n========================================\n")
+        print(f"\n========================================")
+        print(f"  running: {self.model} on {self.host}")
+        print(f"===============[History]================")
         for line in self.msgs:
-            print(f"{line['role']}: {line['content']}\n")
-        print(f"\n========================================\n")
+            print(f"  | {line['role']}: {line['content']} |")
+        print(f"========================================")
+    
+    def help(self):
+        print(
+                f"\n"+
+                "Useage:\n\n"+
+                "   [prompt]                to Chat\n\n"+
+                "   /exit or /quit          to Exit program\n"+
+                "   /set [addr] [port]      to Change settings\n"+
+                "   /run [model]            to Change model to use\n"+
+                "   /show                   to List Models can be used\n"+
+                "   /help or /?             to Show this list of commands"
+            )
 
 def main():
     cli = client()
+    cli.draw()
+    cli.help()
 
     while True:
         cli.get_input()
-        cli.chat()
-
 
 if __name__ == "__main__":
     main()
